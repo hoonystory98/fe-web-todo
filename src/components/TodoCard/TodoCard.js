c00403726822ea236415ed6da45e7e1b0cc1ab16
RandomlyChristen <yylwd2000@naver.com>
2023-01-11 11:13:59 +0900

    Refactor: TodoCard 가 edit 중 일때의 스타일 변화를 css로 분리
    그 외 편집 테두리 디자인 추가

--- src/components/TodoCard/TodoCard.js
+++ src/components/TodoCard/TodoCard.js
@@ -15,14 +15,43 @@ class TodoCard extends Component {
         this.addEvent('click', '.todocard-bgbtn', this.cancelEdit.bind(this));
     }
 
+    template() {
+        const { todo, isEdit } = this.state;
+        return `
+        <button class="todocard-bgbtn"></button>
+        <div class="todocard-dblclick-area">
+            <div class="todocard-header">
+                <input class="todocard-title" value="${todo.name}" ${isEdit || 'disabled'}>
+                <button class="todocard-delete" ${!isEdit || 'disabled'}>
+                    <svg width="12" height="12" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1.5 11.25L0.75 10.5L5.25 6L0.75 1.5L1.5 0.750004L6 5.25L10.5 0.750004L11.25 1.5L6.75 6L11.25 10.5L10.5 11.25L6 6.75L1.5 11.25Z"/>
+                    </svg>
+                </button>            
+            </div>
+            ${isEdit ?
+            `<textarea class="todocard-desc">${todo.description}</textarea>` :
+            `<pre class="todocard-desc">${todo.description}</pre>`
+            }
+            <div class="todocard-btn-area">
+                <button class="todocard-edit-cancel">취소</button>
+                <button class="todocard-edit-ok">수정</button>
+            </div>        
+        </div>
+        `
+    }
+
+    mounted() {
+        this.renderDraggable();
+        this.renderEdit();
+        this.fitHeight();
+    }
+
     startEdit() {
         this.setState({ isEdit: true });
     }
-
     cancelEdit() {
         this.setState({ isEdit: false });
     }
-
     finishEdit() {
         const $title = this.$target.querySelector('.todocard-title');
         const $desc = this.$target.querySelector('.todocard-desc');
@@ -37,36 +66,19 @@ class TodoCard extends Component {
         });
         TodoDatabase.updateTodo(newTodo);
     }
-
-    template() {
-        const { todo, isEdit } = this.state;
-        return `
-        ${isEdit ? `<button class="todocard-bgbtn"></button>`: ''}
-        <div class="todocard-dblclick-area" ${!isEdit || 'style="z-index: 71"'}>
-            <div class="todocard-header">
-                <input class="todocard-title" value="${todo.name}" ${isEdit || 'disabled'}>
-                <button class="todocard-delete" ${!isEdit || 'disabled'}>
-                    <svg width="12" height="12" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1.5 11.25L0.75 10.5L5.25 6L0.75 1.5L1.5 0.750004L6 5.25L10.5 0.750004L11.25 1.5L6.75 6L11.25 10.5L10.5 11.25L6 6.75L1.5 11.25Z"/>
-                    </svg>
-                </button>            
-            </div>
-            ${isEdit ?
-            `<textarea class="todocard-desc">${todo.description}</textarea>` :
-            `<pre class="todocard-desc">${todo.description}</pre>`
-            }
-            <div class="todocard-btn-area" style="${isEdit ? 'height: auto' : 'height: 0'}; overflow: hidden">
-                <button class="todocard-edit-cancel">취소</button>
-                <button class="todocard-edit-ok">수정</button>
-            </div>        
-        </div>
-        `
-    }
-
-    mounted() {
+    fitHeight() {
         const $textarea = this.$target.querySelector('.todocard-desc');
         $textarea.style.height = `${$textarea.scrollHeight}px`;
+    }
+    renderDraggable() {
         this.$target.draggable = !this.state.isEdit;
+    }
+    renderEdit() {
+        if (this.state.isEdit) {
+            this.$target.classList.add('edit');
+        } else {
+            this.$target.classList.remove('edit');
+        }
     }
 }
 
